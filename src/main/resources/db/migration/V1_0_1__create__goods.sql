299ecbabd5e58b47b5e1a1a7368dd29e0ee01c7c
WeiDapeng <dpwei@thoughtworks.com>
2018-01-14 16:59:19 +0800

    [Dapeng] add goods table

---
+++ src/main/resources/db/migration/V1_0_1__create__goods.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE `goods` (
+  `id`             BIGINT       NOT NULL AUTO_INCREMENT,
+  `name`           VARCHAR(50)  NOT NULL,
+  `price`          FLOAT        NOT NULL,
+  `description`    VARCHAR(200) NULL,
+  `stock_quantity` INT          NULL,
+  PRIMARY KEY (`id`)
+);
