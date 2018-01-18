5e1234487dcd4be71541e30f0b778eb157639096
WeiDapeng <dpwei@thoughtworks.com>
2018-01-18 19:57:28 +0800

    [Dapeng] add feign client support

--- src/main/java/com/thoughtworks/mstorderservice/MstGoodsServiceApplication.java
+++ src/main/java/com/thoughtworks/mstorderservice/MstGoodsServiceApplication.java
@@ -3,9 +3,11 @@ package com.thoughtworks.mstorderservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class MstGoodsServiceApplication {
 
     public static void main(String[] args) {
