0cd975cf3a5b55b6f37a747214994ff8d426bea7
WeiDapeng <dpwei@thoughtworks.com>
2018-01-18 19:32:18 +0800

    [Dapeng] add consul and health check.

--- src/main/java/com/thoughtworks/mstorderservice/MstOrderServiceApplication.java
+++ src/main/java/com/thoughtworks/mstorderservice/MstOrderServiceApplication.java
@@ -2,11 +2,13 @@ package com.thoughtworks.mstorderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MstOrderServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MstOrderServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MstOrderServiceApplication.class, args);
+    }
 }
